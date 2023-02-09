2dd57512c82bdb51f9456b94af1800bfb460a3b0
ericcnguyen04 <ericc.win@gmail.com>
2023-02-08 19:54:40 -0500

    the debug

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import Navbar from '@/components/Navbar'
+import NavBar from '@/components/Navbar'
 
 export default function App({ Component, pageProps }) {
   return (
